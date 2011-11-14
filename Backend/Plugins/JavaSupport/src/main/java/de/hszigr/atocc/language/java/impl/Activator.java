aa3035b50fb5a030e08389049a9665b89bbb27ba
Stefan Bradl <stefan_bradl@web.de>
2011-11-14 16:36:34 +0100

    -

---
+++ Backend/Plugins/JavaSupport/src/main/java/de/hszigr/atocc/language/java/impl/Activator.java
@@ -0,0 +1,17 @@
+package de.hszigr.atocc.language.java.impl;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+    public void start(BundleContext context) throws Exception {
+        System.out.println("START JavaSupport");
+    }
+
+    public void stop(BundleContext context) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
