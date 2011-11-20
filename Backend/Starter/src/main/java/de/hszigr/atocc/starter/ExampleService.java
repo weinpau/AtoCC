e978fae6908d38972b068a3b7bd5b58703a5fb14
Stefan Bradl <stefan_bradl@web.de>
2011-11-20 22:51:58 +0100

    created starter bundle

---
+++ Backend/Starter/src/main/java/de/hszigr/atocc/starter/ExampleService.java
@@ -0,0 +1,12 @@
+package de.hszigr.atocc.starter;
+
+/**
+ * Public API representing an example OSGi service
+ */
+public interface ExampleService
+{
+    // public methods go here...
+
+    String scramble( String text );
+}
+
