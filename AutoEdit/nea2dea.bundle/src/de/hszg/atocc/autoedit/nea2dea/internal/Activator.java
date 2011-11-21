9ef44b04c8e2a269d597b6ad9c96720ca5706543
Stefan Bradl <stefan_bradl@web.de>
2011-11-21 20:04:58 +0100

    Test features and bundles created

---
+++ AutoEdit/nea2dea.bundle/src/de/hszg/atocc/autoedit/nea2dea/internal/Activator.java
@@ -0,0 +1,24 @@
+package de.hszg.atocc.autoedit.nea2dea.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Hello Nea2Dea!!");
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Goodbye Nea2Dea!!");
+	}
+
+}
