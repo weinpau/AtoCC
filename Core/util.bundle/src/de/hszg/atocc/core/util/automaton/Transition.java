245c3e790fa6ced23fd173efff7360fe94be4774
Stefan Bradl <stefan_bradl@web.de>
2011-12-02 21:34:08 +0100

    Implemented equals and hashCode for Transtions to allow checking sets of transitions for equality

--- Core/util.bundle/src/de/hszg/atocc/core/util/automaton/Transition.java
+++ Core/util.bundle/src/de/hszg/atocc/core/util/automaton/Transition.java
@@ -24,4 +24,36 @@ public final class Transition {
         return characterToRead;
     }
 
+    @Override
+    public String toString() {
+        return String.format("(%s, %s) = %s", source, characterToRead, target);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((characterToRead == null) ? 0 : characterToRead.hashCode());
+        result = prime * result + ((source == null) ? 0 : source.hashCode());
+        result = prime * result + ((target == null) ? 0 : target.hashCode());
+        
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof Transition)) {
+            return false;
+        }
+
+        final Transition transition = (Transition) obj;
+
+        return source.equals(transition.source) && target.equals(transition.target)
+                && characterToRead.equals(transition.characterToRead);
+    }
+    
 }
