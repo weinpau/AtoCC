04e11acf214aa9d512c84225b1c9c78a53f8f842
Stefan Bradl <stefan_bradl@web.de>
2011-12-04 17:27:02 +0100

    -

--- AutoEdit/nea2dea.bundle/src/de/hszg/atocc/autoedit/nea2dea/internal/Nea2DeaService.java
+++ AutoEdit/nea2dea.bundle/src/de/hszg/atocc/autoedit/nea2dea/internal/Nea2DeaService.java
@@ -2,20 +2,7 @@ package de.hszg.atocc.autoedit.nea2dea.internal;
 
 import de.hszg.atocc.core.pluginregistry.AbstractServiceComponent;
 import de.hszg.atocc.core.pluginregistry.WebService;
-import de.hszg.atocc.core.util.XmlValidatorService;
 
 @WebService(url = "/autoedit/nea2dea", resource = Nea2Dea.class)
 public final class Nea2DeaService extends AbstractServiceComponent {
-
-    private XmlValidatorService xmlValidator;
-
-    public synchronized void setXmlValidatorService(XmlValidatorService service) {
-        xmlValidator = service;
-    }
-
-    public synchronized void unsetXmlValidatorService(XmlValidatorService service) {
-        if (xmlValidator == service) {
-            xmlValidator = null;
-        }
-    }
 }
