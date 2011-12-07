a11612ec26b1d4460bcb64b6eabe068d41c09709
Stefan Bradl <stefan_bradl@web.de>
2011-12-07 09:27:40 +0100

    added test stubs

---
+++ Core/util.test-bundle/src/de/hszg/atocc/core/util/test/AutomatonTests.java
@@ -0,0 +1,99 @@
+package de.hszg.atocc.core.util.test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class AutomatonTests {
+
+    @Test
+    public void testAutomaton() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testContainsEpsilonRules() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetType() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetAlphabet() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddAlphabetItem() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetAlphabet() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetStates() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddState() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetStates() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetTransitionsFor() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetTargetsFor() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetTransitions() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddTransition() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetInitialState() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetInitialState() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetFinalStates() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddFinalState() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testEqualsObject() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ Core/util.test-bundle/src/de/hszg/atocc/core/util/test/RestfulWebServiceTests.java
@@ -0,0 +1,19 @@
+package de.hszg.atocc.core.util.test;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+
+public class RestfulWebServiceTests {
+
+    @Test
+    public void testDoInit() {
+        Assert.fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetService() {
+        Assert.fail("Not yet implemented");
+    }
+
+}
