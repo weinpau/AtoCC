0fdd72ba69999ea22d333796b8abc5b41d8b98a3
Stefan Bradl <stefan_bradl@web.de>
2011-12-22 17:50:14 +0100

    Implemented transition tests

--- Core/util.test-bundle/src/de/hszg/atocc/core/util/test/TransitionTests.java
+++ Core/util.test-bundle/src/de/hszg/atocc/core/util/test/TransitionTests.java
@@ -1,38 +1,65 @@
 package de.hszg.atocc.core.util.test;
 
+import de.hszg.atocc.core.util.automaton.Transition;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public final class TransitionTests {
 
     @Test
-    public void testTransition() {
-        Assert.fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetSource() {
-        Assert.fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetTarget() {
-        Assert.fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetCharacterToRead() {
-        Assert.fail("Not yet implemented");
+    public void testConstructor() {
+        Transition transition = new Transition("q0", "q1", "a");
+        
+        Assert.assertEquals("q0", transition.getSource());
+        Assert.assertEquals("q1", transition.getTarget());
+        Assert.assertEquals("a", transition.getCharacterToRead());
     }
 
     @Test
     public void testToString() {
-        Assert.fail("Not yet implemented");
+        Transition transition = new Transition("q0", "q1", "a");
+        
+        Assert.assertEquals("(q0, a) = q1", transition.toString());
     }
 
     @Test
-    public void testEqualsObject() {
-        Assert.fail("Not yet implemented");
+    public void equalsShouldReturnTrueIfAllPropertiesAreEqual() {
+        Transition t1 = new Transition("q0", "q1", "a");
+        Transition t2 = new Transition("q0", "q1", "a");
+        
+        Assert.assertTrue(t1.equals(t2));
+    }
+    
+    @Test
+    public void equalsShouldReturnFalsIsSourceStatesAreNotEqual() {
+        Transition t1 = new Transition("q0", "q1", "a");
+        Transition t2 = new Transition("q2", "q1", "a");
+        
+        Assert.assertFalse(t1.equals(t2));
+    }
+    
+    @Test
+    public void equalsShouldReturnFalsIsTargetStatesAreNotEqual() {
+        Transition t1 = new Transition("q0", "q1", "a");
+        Transition t2 = new Transition("q0", "q2", "a");
+        
+        Assert.assertFalse(t1.equals(t2));
+    }
+    
+    @Test
+    public void equalsShouldReturnFalsIsCharactersToReadAreNotEqual() {
+        Transition t1 = new Transition("q0", "q1", "a");
+        Transition t2 = new Transition("q0", "q1", "b");
+        
+        Assert.assertFalse(t1.equals(t2));
+    }
+    
+    @Test
+    public void equalsShouldReturnFalseIfOtherObjectIsNoTransition() {
+        Transition transition = new Transition("q0", "q1", "a");
+        
+        Assert.assertFalse(transition.equals("other"));
     }
 
 }
