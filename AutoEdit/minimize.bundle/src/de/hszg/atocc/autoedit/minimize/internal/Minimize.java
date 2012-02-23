fefba238840bc0ca4ee10fde8e71332a8e8fdc84
Stefan Bradl <stefan_bradl@web.de>
2012-02-23 16:44:32 +0800

    add project for "minimize automaton"

---
+++ AutoEdit/minimize.bundle/src/de/hszg/atocc/autoedit/minimize/internal/Minimize.java
@@ -0,0 +1,7 @@
+package de.hszg.atocc.autoedit.minimize.internal;
+
+import de.hszg.atocc.core.util.RestfulWebService;
+
+public final class Minimize extends RestfulWebService {
+
+}
---
+++ AutoEdit/minimize.bundle/src/de/hszg/atocc/autoedit/minimize/internal/MinimizeService.java
@@ -0,0 +1,9 @@
+package de.hszg.atocc.autoedit.minimize.internal;
+
+import de.hszg.atocc.core.pluginregistry.AbstractServiceComponent;
+import de.hszg.atocc.core.pluginregistry.WebService;
+
+@WebService(url = "/autoedit/minimize", resource = Minimize.class)
+public class MinimizeService extends AbstractServiceComponent {
+
+}
