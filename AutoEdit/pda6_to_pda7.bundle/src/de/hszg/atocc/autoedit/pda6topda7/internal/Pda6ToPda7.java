f29a5b57ee700b3966aad05025a32e02e7f1a5a6
Stefan Bradl <stefan_bradl@web.de>
2012-03-08 16:58:11 +0800

    project skeleton created

---
+++ AutoEdit/pda6_to_pda7.bundle/src/de/hszg/atocc/autoedit/pda6topda7/internal/Pda6ToPda7.java
@@ -0,0 +1,13 @@
+package de.hszg.atocc.autoedit.pda6topda7.internal;
+
+import de.hszg.atocc.core.util.AbstractXmlTransformationService;
+import de.hszg.atocc.core.util.XmlTransormationException;
+
+public final class Pda6ToPda7 extends AbstractXmlTransformationService {
+
+    @Override
+    protected void transform() throws XmlTransormationException {
+
+    }
+
+}
---
+++ AutoEdit/pda6_to_pda7.bundle/src/de/hszg/atocc/autoedit/pda6topda7/internal/Pda6ToPda7Component.java
@@ -0,0 +1,9 @@
+package de.hszg.atocc.autoedit.pda6topda7.internal;
+
+import de.hszg.atocc.core.pluginregistry.AbstractServiceComponent;
+import de.hszg.atocc.core.pluginregistry.WebService;
+
+@WebService(url = "/autoedit/pda6topda7", resource = Pda6ToPda7.class)
+public final class Pda6ToPda7Component extends AbstractServiceComponent {
+
+}
